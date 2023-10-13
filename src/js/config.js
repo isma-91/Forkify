3e1337b4a76a12abd14e794ff0ba7df1f3ae2000
isma-91 <isma91@hotmail.it>
2023-10-13 09:48:26 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '946aa1fa-27e5-40fd-bcf7-63746dd84caa';
+export const MODAL_CLOSE_SEC = 2.5;
